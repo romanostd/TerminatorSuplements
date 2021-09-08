6df2d19fdf70eecc74a7281f41691a749c94bc1f
Romano M. Stedile <54461411+romanostd@users.noreply.github.com>
2021-09-07 23:27:57 -0300

    configuring data base

---
+++ backend/knexfile.js
@@ -0,0 +1,17 @@
+const { db } = require('./.env')
+
+module.exports = {
+	client: 'postgresql',
+	connection: {
+		database: 'terminatordb',
+		user: 'oostgres',
+		password:'123456'
+	},
+	pool: {
+		min: 2,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
+};
