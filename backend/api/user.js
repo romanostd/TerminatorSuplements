a9460daf7e05ad9bf6c1f9f75d1e587f903d4bf0
Romano M. Stedile <54461411+romanostd@users.noreply.github.com>
2022-05-28 22:30:32 -0300

    .

--- backend/api/user.js
+++ backend/api/user.js
@@ -10,36 +10,33 @@ module.exports = app => {
 
     const save = async (req, res) => {
         const user = { ...req.body }
-        if (req.params.id) user.id = req.params.id
+        if(req.params.id) user.id = req.params.id
 
-        if (!req.originalUrl.startsWith('/users')) user.admin = false
-        if (!req.user || !req.user.admin) user.admin = false
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
 
         try {
             existsOrError(user.nome, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
             existsOrError(user.password, 'Senha não informada')
-            // existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
-            // equalsOrError(user.password, user.confirmPassword,
-                // 'Senhas não conferem')
 
             const userFromDB = await app.db('users')
                 .where({ email: user.email }).first()
-            if (!user.id) {
+            if(!user.id) {
                 notExistsOrError(userFromDB, 'Usuário já cadastrado')
             }
-        } catch (msg) {
+        } catch(msg) {
             return res.status(400).send(msg)
         }
 
         user.password = encryptPassword(user.password)
         delete user.confirmPassword
 
-        if (user.id) {
+        if(user.id) {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
-                // .whereNull('deletedAt')
+                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
