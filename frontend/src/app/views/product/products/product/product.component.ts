2b67cd281aa9bc48df68f7dc6ecca4097af23bc2
romanostd <romanostedile@gmail.com>
2023-03-29 15:26:32 -0300

    fix: more validations added

--- frontend/src/app/views/product/products/product/product.component.ts
+++ frontend/src/app/views/product/products/product/product.component.ts
@@ -1,6 +1,6 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
@@ -26,21 +26,18 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
 
     if (this.data != undefined) {
-      this.productService.getById(this.data.id).subscribe(product => {
-        this.product = product
-      })
       this.title = 'EDITAR PRODUTO'
     }
   }
 
   form: FormGroup = this.fb.group({
 
-    name: [this.data?.name, this.product?.name],
-    descreption: [this.data?.descreption, this.product?.descreption],
-    imageUrl: [this.data?.imageUrl, this.product?.imageUrl],
-    price: [this.data?.price, this.product?.price],
-    categoryId: [this.data?.categoryId, this.product?.categoryId],
-    userId: [this.data?.categoryId, this.product?.userId],
+    name: [this.data?.name, Validators.required],
+    descreption: [this.data?.descreption, Validators.required],
+    imageUrl: [this.data?.imageUrl, Validators.required],
+    price: [this.data?.price, Validators.required],
+    categoryId: [this.data?.categoryId, Validators.required],
+    userId: [this.data?.categoryId, Validators.required],
   })
 
 
--- frontend/src/app/views/user/users/user/user.component.ts
+++ frontend/src/app/views/user/users/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,13 +13,14 @@ import { UserService } from 'src/app/services/user.service';
 export class UserComponent implements OnInit {
 
   constructor(
+    public router: Router,
     private fb: FormBuilder,
     private userService: UserService,
     public dialogRef: MatDialogRef<UserComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
 
   ) { }
-
+  admin = new FormControl();
   user?: User
   title = 'CREATE USER'
 
@@ -29,16 +31,17 @@ export class UserComponent implements OnInit {
   }
 
   form: FormGroup = this.fb.group({
-    name: [ this.data?.nome, Validators.required],
+    nome: [ this.data?.nome, Validators.required],
     email: [this.data?.email, Validators.compose(
       [Validators.email, Validators.required])],
     password: [this.data?.password, Validators.required],
-    admin: [this.data?.admin, Validators.required],
+    admin: [this.data?.admin],
   })
 
 
   async saveUser() {
     if (this.data != undefined) {
+      this.form.controls.admin.value == 'true' ? this.form.controls.admin.setValue(true) : this.form.controls.admin.setValue(false)
       await this.userService.put(this.form.value, this.data.id);
     }
     else {
