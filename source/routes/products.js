b2c203f676e967f3d6776d9508a466059373fa41
romanostd <romanostedile@gmail.com>
2023-04-10 23:27:32 -0300

    feat: products new api added

--- source/routes/products.js
+++ source/routes/products.js
@@ -1,21 +1,158 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mysql = require("../mysql").pool;
 
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        messege: 'using get products route'
-    })
+router.get("/", (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: errror });
+    }
+    conn.query("SELECT * FROM products;", (error, result, field) => {
+      conn.release();
+
+      if (error) {
+        res.status(500).send({
+          error: error,
+          response: null,
+        });
+      }
+      return res.status(200).send({ response: result });
+    });
+  });
 });
 
-router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
+router.get("/:product_id", (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: errror });
     }
-    res.status(201).send({
-        messege: 'Product insert',
-        createdProduct: product
-    })
-})
+    conn.query(
+      "SELECT * FROM products WHERE product_id = ?;",
+      [req.params.product_id],
+      (error, result, field) => {
+        conn.release();
+
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        return res.status(200).send({ response: result });
+      }
+    );
+  });
+});
+
+router.post("/", (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      "INSERT INTO products (name, price, description, imageUrl, quantity) VALUES (?,?,?,?,?)",
+      [
+        req.body.name,
+        req.body.price,
+        req.body.description,
+        req.body.imageUrl,
+        req.body.quantity,
+      ],
+      (error, result, field) => {
+        conn.release();
+
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(201).send({
+          massage: "Product created successfully",
+          product_id: result.insertId,
+        });
+      }
+    );
+  });
+  res.status(201).send({
+    messege: "Product insert",
+    createdProduct: product,
+  });
+});
+
+router.patch("/", (req, res, next) => {
+    const product = {
+      name: req.body.name,
+      price: req.body.price,
+    };
+  
+    mysql.getConnection((error, conn) => {
+      if (error) {
+        return res.status(500).send({ error: error });
+      }
+      conn.query(
+        `UPDATE products SET name = ?, price = ?, description = ?, imageUrl = ?, quantity = ? WHERE product_id = ?`,
+        [
+          req.body.name,
+          req.body.price,
+          req.body.description,
+          req.body.imageUrl,
+          req.body.quantity,
+          req.body.product_id
+        ],
+        (error, result, field) => {
+          conn.release();
+  
+          if (error) {
+            res.status(500).send({
+              error: error,
+              response: null,
+            });
+          }
+          res.status(202).send({
+            massage: "Product updated successfully",
+          });
+        }
+      );
+    });
+  });
+
+
+  router.delete("/", (req, res, next) => {
+    const product = {
+      name: req.body.name,
+      price: req.body.price,
+    };
+  
+    mysql.getConnection((error, conn) => {
+      if (error) {
+        return res.status(500).send({ error: error });
+      }
+      conn.query(
+        `DELETE FROM products WHERE product_id = ?`,
+        [
+          req.body.product_id
+        ],
+        (error, result, field) => {
+          conn.release();
+  
+          if (error) {
+            res.status(500).send({
+              error: error,
+              response: null,
+            });
+          }
+          res.status(202).send({
+            massage: "Product removed successfully",
+          });
+        }
+      );
+    });
+  });
 
 module.exports = router;
