e995c0bbbab440f3f9415626b8f2d969a2af456e
romanostd <romanostedile@gmail.com>
2023-10-30 15:34:09 -0300

    some configs added

--- source/server.js
+++ source/server.js
@@ -2,4 +2,15 @@ const http = require("http");
 const app = require("./app");
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(port);
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+server.on("error", error => {
+  console.error(`An error occurred while starting the server: ${error}`);
+});
+
+server.on("close", () => {
+  console.log("Server has been closed");
+});
