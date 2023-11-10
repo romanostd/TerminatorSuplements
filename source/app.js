734bd4e54205284a276e38f6a4b95ee7f136796c
romanostd <romanostedile@gmail.com>
2023-11-10 09:33:17 -0300

    app fixed

--- source/app.js
+++ source/app.js
@@ -1,4 +1,4 @@
-require("./dotenv");
+require('dotenv').config(); 
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   );
 
