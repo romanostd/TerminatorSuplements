47af185b15d971e3fb7855c83eaf9c882d6956f4
romanostd <romanostedile@gmail.com>
2023-12-01 14:27:37 -0300

    prod config for front updated

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://terminator-lb-prod-997927481.us-east-2.elb.amazonaws.com/",
+  apiUrl: "http://terminator-lb-prod-997927481.us-east-2.elb.amazonaws.com",
 };
